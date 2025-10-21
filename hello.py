441dc3622b560fc736d553c115e9f0c0bbd4f46c
bashkip <sebastiaanhelbing@gmail.com>
2025-10-21 10:58:15 +0200

    check hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,84 @@
-#yooo family
+# Necessary Imports
+import torch
+import pandas
+import numpy
+from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification, Trainer, TrainingArguments
+from datasets import load_dataset
+from datasets import Dataset
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = logits.argmax(axis=-1)
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, predictions, average='binary')
+    acc = accuracy_score(labels, predictions)
+    return {
+        'accuracy': acc,
+        'f1': f1,
+        'precision': precision,
+        'recall': recall
+    }
+
+# For GPU: Remvoe this line and to(device) in model definition. also change no_cuda to False in Training arguments
+#device = torch.device('cpu')
+
+
+# Split data into training and test set
+train_df = df.sample(frac=0.8, random_state=42)  # 80% for training
+test_df = df.drop(train_df.index)
+
+train_dataset = Dataset.from_pandas(train_df)
+test_dataset = Dataset.from_pandas(test_df)
+
+# Load the BERT tokenizer
+tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+
+#tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+# Tokenize the dataset
+def tokenize_function(examples):
+    return tokenizer(examples['text'], padding='max_length', truncation=True, max_length=256)
+
+tokenized_train_dataset = train_dataset.map(tokenize_function, batched=True)
+tokenized_test_dataset = test_dataset.map(tokenize_function, batched=True)
+
+# Load the BERT model for sequence classification
+model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=2).to(device) # 2 labels: pos and neg
+
+#model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2).to(device) # 2 labels: pos and neg
+
+# Define training arguments and set up Trainer
+training_args = TrainingArguments(
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    num_train_epochs=1,
+    evaluation_strategy="epoch",
+    logging_dir="./logs",
+    logging_steps=500,
+    do_train=True,
+    do_eval=True,
+    #no_cuda=True,
+    load_best_model_at_end=True,
+    save_strategy="epoch",
+    report_to="tensorboard", # for logging to TensorBoard
+    output_dir="./results",
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_train_dataset,
+    eval_dataset=tokenized_test_dataset,
+    tokenizer=tokenizer,
+    compute_metrics=compute_metrics
+)
+
+# Train the model
+trainer.train()
+
+# Evaluate the model
+results = trainer.evaluate()
+
+print(results)
